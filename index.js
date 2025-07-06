4c56df36060cfe79f6734c7ea9392a6f539dbe36
AliasUnknown710 <alexliss216@gmail.com>
2025-07-06 17:48:01 -0400

    File Updates

--- index.js
+++
@@ -1,7 +0,0 @@
-import { fetch as profileFetch } from './ProfileInfoWorker.js';
-
-export default {
-    async fetch(request, env, ctx) {
-        return await profileFetch(request, env, ctx);
-    }
-};
